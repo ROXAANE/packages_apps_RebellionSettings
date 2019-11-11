c8b161b406b49a9967fe013ef2dc944a4d0f2cf3
Christian Oder <myself5@carbonrom.org>
2019-11-11 22:06:13 +0530

    Rebellion-Hub: Readd Navbar Layout selection
    Change-Id: Iae6bbef371f09cd2bf71763e5aeef2461f31e35c
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- src/com/rebellion/settings/fragments/Navigation.java
+++ src/com/rebellion/settings/fragments/Navigation.java
@@ -16,6 +16,8 @@
 package com.rebellion.settings.fragments;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -33,14 +35,35 @@ public class Navigation extends SettingsPreferenceFragment
 
     public static final String TAG = "Navigation";
 
+    private static final String NAV_BAR_LAYOUT = "nav_bar_layout";
+    private static final String SYSUI_NAV_BAR = "sysui_nav_bar";
+
+    private ListPreference mNavBarLayout;
+    private ContentResolver mResolver;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.rebellion_settings_navigation);
+
+        mResolver = getActivity().getContentResolver();
+
+        mNavBarLayout = (ListPreference) findPreference(NAV_BAR_LAYOUT);
+        mNavBarLayout.setOnPreferenceChangeListener(this);
+        String navBarLayoutValue = Settings.Secure.getString(mResolver, SYSUI_NAV_BAR);
+        if (navBarLayoutValue != null) {
+            mNavBarLayout.setValue(navBarLayoutValue);
+        } else {
+            mNavBarLayout.setValueIndex(0);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mNavBarLayout) {
+            Settings.Secure.putString(mResolver, SYSUI_NAV_BAR, (String) newValue);
+            return true;
+        }
         return false;
     }
 
